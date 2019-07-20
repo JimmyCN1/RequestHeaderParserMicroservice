e526d71c3acd4c5a9e73be733b29bcf5d0e519bd
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-20 16:10:02 +1000

    implemented header parser

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.get("/api/hello", function(req, res) {
 });
 
 //
-app.get("/api/whoami", (req, res) => {});
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    // object: req.headers,
+    ipaddress: req.connection.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
